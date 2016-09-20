6c33282f6e9bd8ef351ae045a6890b1328f2032a
Pigggger <296056428@qq.com>
2016-09-21 00:15:30 +0800

    init

---
+++ API_Demos/Assets/Scrips/SetFromToRotation.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetFromToRotation : MonoBehaviour
+{
+
+    public Transform A, B, C;
+    private Quaternion q1 = Quaternion.identity;
+
+    void Update()
+    {
+        q1.SetFromToRotation(A.position, B.position);
+        //相当于站在A点 以C的X正轴为眼，朝B的方向看
+
+        C.rotation = q1;
+
+        Debug.DrawLine(Vector3.zero,A.position,Color.red);
+        Debug.DrawLine(Vector3.zero, B.position, Color.green);
+        Debug.DrawLine(C.position ,C.position+new Vector3(0.0f,1.0f,0.0f), Color.black);
+    }
+}
